a0a5a52fb6a83df73c58d2463ff4c29e0769ac7e
Joshua Daniel <Joshua_dup@hotmail.com>
2022-10-20 23:43:01 -0500

    diseño responsive de 800

---
+++ index.js
@@ -0,0 +1,10 @@
+window.addEventListener("DOMContentLoaded", (event)=> {
+    console.log("Dom complated")
+    const menuBtn = document.getElementById("menu");
+    const nav = document.querySelector("nav list-nav");
+
+    menuBtn.addEventListener("click", (event) => {
+        menuBtn.classList.toggle("salir");
+        nav.classList.toggle("visible");
+    })
+})
